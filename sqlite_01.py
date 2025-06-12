eb488f0f0f837cced81310263e04198532a856ba
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-06-12 10:58:38 -0400

    Update sqlite_01.py

--- sqlite_01.py
+++ sqlite_01.py
@@ -4,9 +4,9 @@ cursor.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@codemy.com'
 
 # inserting many values in customers
 many_customers=[
-  ('name_1', 'last_name_1', 'email_1'),
-  ('name_2', 'last_name_2', 'email_2'),
-  ('name_3', 'last_name_3', 'email_3'),
+  ('name_1', 'last_name_1', 'name1@mail.com'),
+  ('name_2', 'last_name_2', 'name2@mail.com'),
+  ('name_3', 'last_name_3', 'name3@mail.com'),
 ]
 cursor.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
 
